f5059b8d09c6bdeaf0275b383ad2436ecab86bce
19smabtahinoor <abtahinoorkabid@gmail.com>
2021-09-17 09:54:40 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-n4wosw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-n4wosw)
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [input, setInput] = useState('');
+  const allColors = [
+    'blue',
+    'green',
+    'red',
+    'yellow',
+    'purple',
+    'gray',
+    'pink',
+    'violet',
+  ];
+
+  return (
+    <div>
+      <h1 className="heading">🚀 Color Searcher</h1>
+      <input
+        className="input_box"
+        type="text"
+        placeholder="Search any color"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <h1 className="heading">{input}</h1>
+
+      <div className="colors">
+        {allColors
+          .filter((color) => color.includes(input.toLowerCase()))
+          .map((color) => {
+            return (
+              <>
+                <div className="each_color" style={{ backgroundColor: color }}>
+                  <h1>{color}</h1>
+                </div>
+              </>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
